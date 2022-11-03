e16b85ef09d25a200a4887de788280ed32ef897c
1187aika <riksp27y@yahoo.co.jp>
2022-11-03 21:43:36 +0900

    add2

--- test1102/src/index.js
+++ test1102/src/index.js
@@ -160,3 +160,11 @@ if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c] && squa
 }
   return null;
 }
+// ========================================
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
